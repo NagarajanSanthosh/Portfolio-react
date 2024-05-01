fb096e4b43ff6eba66814cbea24af51600a68131
NagarajanSanthosh <64208ns@gmail.com>
2024-05-01 23:19:58 +0530

    removed custom class name "row" "col" . instead brought a predefined classes from bootstrap

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,31 +1,41 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../about.css'
+import aboutImage from '../image/web-fotor.jpg'
+import { Container, Row, Col } from 'react-bootstrap'
 const About = () => {
   return (
-    <div id='about'>
+    <div id='about' className='bottom-style'>
       <div>
-        <p className='text-starter'>Get to Know More</p>
-        <p className='title'>
+        <p className='text-starter text-description'>Get to Know More</p>
+        <h5 className='title'>
           About
-        </p>
+        </h5>
       </div>
       <div >
 
-        <div>
-          <div>
-            <h5>Education</h5>
-            <p>B.Tech. Bachelor's Degree</p>
-            <p>H.S.C. Higher Secondary</p>
-          </div>
-          <div>
-            <p>I am a self-motivated, final-year student with bachelor's degree. I am looking forward to contribute to the growing industry with my potential skills.</p>
+        <Container>
+          <Row>
+            <Col>
+              <img src={aboutImage} alt='about-image' className='image-container' />
+            </Col>
+            <Col className='text-container'>
+              <div>
+                <h5>Education</h5>
+                <p>B.Tech. Bachelor's Degree</p>
+                <p>H.S.C. Higher Secondary</p>
+              </div>
+              <div>
+                <p>I am a self-motivated, final-year student with bachelor's degree. I am looking forward to contribute to the growing industry with my potential skills.</p>
+              </div>
+            </Col>
 
-          </div>
-        </div>
+          </Row>
+
+        </Container>
 
       </div>
-    </div>
+    </div >
   )
 }
 
