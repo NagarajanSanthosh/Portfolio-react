b4008ee4e3f9c86f0ef22363ca0054f8461e3e34
NagarajanSanthosh <64208ns@gmail.com>
2024-05-03 01:13:01 +0530

    skills component updated with bootstrap-responsive styles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,43 +1,49 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../skills.css'
 
 const Skills = () => {
   return (
-    <div id='skills' className='mt-4 mb-4'>
-      <p className='fs-5 text-center'>Explore My Skills</p>
-      <h2 className='text-center mb-4'>Skills</h2>
-
-      <Container>
-        <Row className='gx-4'>
-          <Col md={6} className='border p-4'>
-            <div className='d-flex flex-column h-100 justify-content-center align-items-start'>
-              <h4>Software Developer</h4>
-              <p>I have always liked writing clean and maintainable code.</p>
-              <p>I like to code in Java, JavaScript, Node.js, C, MySQL, Python.</p>
-              <h5>Tools</h5>
-              <ul>
-                <li>Vs Code</li>
-                <li>IntelliJ IDEA</li>
-                <li>Eclipse</li>
-                <li>Google Colab</li>
-              </ul>
-            </div>
-          </Col>
-          <Col md={6} className='border p-4'>
-            <div className='d-flex flex-column h-100 justify-content-center align-items-start'>
-              <h4>Front-end Developer</h4>
-              <p>I enjoy creating visually appealing transitions and effects on the frontend and bringing new concepts to life.</p>
-              <h5>Skills</h5>
-              <p>HTML, CSS, JavaScript, Bootstrap, React.js, Redux, Firebase, Git.</p>
-              <h5>Tools</h5>
-              <ul>
-                <li>Vs Code</li>
-                <li>GitHub</li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+    <div className='skill-section' style={{ color: '#000', padding: '2rem', minHeight: '100vh', marginTop: '2rem' }}>
+      <div id='skills'>
+        <Container fluid>
+          <Row className='justify-content-center'>
+            <Col xs={12} className='text-center mb-4'>
+              <p className='intro-text'>Explore My</p>
+              <h5 className='text-center mb-4'>Skills</h5>
+            </Col>
+          </Row>
+          <Row className='align-items-center my-4'>
+            <Col lg={6} md={6}>
+              <div className='dfWTkB'>
+                <h4>Software Developer</h4>
+                <p>I have always liked writing clean and maintainable code.</p>
+                <p>I like to code in Java, JavaScript, Node.js, C, MySQL, Python.</p>
+                <h5>Tools</h5>
+                <ul>
+                  <li>Vs Code</li>
+                  <li>IntelliJ IDEA</li>
+                  <li>Eclipse</li>
+                  <li>Google Colab</li>
+                </ul>
+              </div>
+            </Col>
+            <Col lg={6} md={6}>
+              <div className='dfWTkB'>
+                <h4>Front-end Developer</h4>
+                <p>I enjoy creating visually appealing transitions and effects on the frontend and bringing new concepts to life.</p>
+                <h5>Skills</h5>
+                <p>HTML, CSS, JavaScript, Bootstrap, React.js, Redux, Firebase, Git.</p>
+                <h5>Tools</h5>
+                <ul>
+                  <li>Vs Code</li>
+                  <li>GitHub</li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   )
 }
